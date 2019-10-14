0ad67aaf5647db19e21d34ebd88b684d1f509cc2
lordgabennewell <ubuntu@ip-172-31-21-114.eu-west-2.compute.internal>
2019-10-14 09:55:13 +0000

    adding cellranger command to .bashrc

--- bootstrap_instance.sh
+++ bootstrap_instance.sh
@@ -5,9 +5,7 @@ mkdir /home/ubuntu/Downloads
 sudo apt-get update
 sudo apt-get install python3.6 python3-pip
 
-#cloning github repo
 cd /home/ubuntu
-git clone https://github.com/lordGabenNewell/BioMage-workload.git
 pip3 install -r BioMage-workload/requirements.txt
 
 cd /home/ubuntu/Downloads
@@ -15,7 +13,9 @@ cd /home/ubuntu/Downloads
 wget -O cellranger-3.1.0.tar.gz "http://cf.10xgenomics.com/releases/cell-exp/cellranger-3.1.0.tar.gz?Expires=1570580309&Policy=eyJTdGF0ZW1lbnQiOlt7IlJlc291cmNlIjoiaHR0cDovL2NmLjEweGdlbm9taWNzLmNvbS9yZWxlYXNlcy9jZWxsLWV4cC9jZWxscmFuZ2VyLTMuMS4wLnRhci5neiIsIkNvbmRpdGlvbiI6eyJEYXRlTGVzc1RoYW4iOnsiQVdTOkVwb2NoVGltZSI6MTU3MDU4MDMwOX19fV19&Signature=areB15CbMneWXcI0W29f5sil3xlIetkaxlFFtlholdiG0FQFfWyxSYJ7OIKyWWxy8tpXmcBQyG84K5UswemBDOko0KndGt~x2boUgCuFE3xg-MVN1Dykir3XTaa7hSWYc9gxo8dmYHMB2FhIuUDO43UEkp35ve5UpT5~sJolKblXMy67N5uIWIcVccCxslVCx0ORSFegG~rmahXbZLPWlIDtdWaC77VpqZo7cKgVAUhQY0egqgnTk9rcgkN1X~wiI~G85gL68UEZcPOy4Wt087izCDWKMXxfYqatsVFbroQKOs6h1lt4bG20z1Rz2sGrLPX4vb3twaqmoXmZ0ipWZw__&Key-Pair-Id=APKAI7S6A5RYOXBWRPDA"
 
 tar -xzvf refdata-cellranger-GRCh38-3.0.0.tar.gz
+
 export PATH=/home/ubuntu/Downloads/cellranger-3.1.0:$PATH
+echo 'export PATH=/home/ubuntu/Downloads/cellranger-3.1.0:$PATH' >> ~/.bashrc
 
 #downloading human reference genome
 wget http://cf.10xgenomics.com/supp/cell-exp/refdata-cellranger-GRCh38-3.0.0.tar.gz
